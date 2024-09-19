91e6f7d2fd6177487d493993d48ac3f5111e762a
Huang Xiwei <huangg.xiwei@gmail.com>
2024-09-18 20:52:05 -0600

    Aprendimos javascript

---
+++ script.js
@@ -0,0 +1,51 @@
+//18 09 2024 /Huang Xiwei
+//comentario en una sola linea
+
+/*
+Comentario 
+multilinea
+.
+.
+.
+.
+wda
+*/
+
+/*
+las variables guardan informaciones que puedes reutilizar
+
+declaracion 
+*/
+
+//declaracion de variable
+
+let nombre = "Xiwei";
+
+const PI = 3.1416;
+
+var edad = 25;
+
+// tipo de datos 
+/*
+cadena de texto(string): "hola mundo"
+
+numero(number) : 5,2,4,2
+
+booleano(boolean) : True o False = 
+*/
+// ejemplo
+
+let mensaje = "holamundo "
+
+let numero = 42
+
+let estudiante = true
+
+//operadores logicas
+//igualdad
+let a = 5
+let b = 2
+let suma = a+b//7
+let resta = a-b//3
+let multiplicacion = a*b//10
+let division = a/b//2.5
