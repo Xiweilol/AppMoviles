c18cb3a4aed0d09821e43f3abe65c9b1103e5ac1
Huang Xiwei <huangg.xiwei@gmail.com>
2024-09-30 20:26:42 -0600

    clase 300924

--- Clase_230924/3.1UsoDeApiwikiPedia.js
+++ Clase_230924/3.1UsoDeApiwikiPedia.js
@@ -1,4 +1,4 @@
-const terminoBusqueda = 'Programación'; // Término que deseas buscar
+const terminoBusqueda = 'MineCraft'; // Término que deseas buscar
 const url = `https://es.wikipedia.org/w/api.php?action=query&list=search&srsearch=${terminoBusqueda}&format=json&origin=*`; 
 
 // Función para obtener resultados de Wikipedia
